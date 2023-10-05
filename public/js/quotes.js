806121bbd74d8c55c2b38113cb0455765103c1e7
o.bogatyreva <bogatireva.o.v@gmail.com>
2023-10-06 02:02:34 +0300

    change color text of quotes

--- public/js/quotes.js
+++ public/js/quotes.js
@@ -9,9 +9,26 @@ let changeBackgroundColor = () => {
         randomIndexColor = Math.floor(Math.random() * colors.length);
     }
     quotePage.css("background-color", colors[randomIndexColor]);
+    quotePage.css("color", colors[randomIndexColor]);
+};
+
+let showNewQuote = () => {
+    let textQuote = $("#text");
+    let authorQuote = $("#author");
+    let randomIndexQuote = Math.floor(Math.random() * quotes.length);
+    let currentQuote = textQuote.text();
+
+    while (currentQuote === quotes[randomIndexQuote].text) {
+        randomIndexQuote = Math.floor(Math.random() * quotes.length);
+    }
+    let newAuthor = quotes[randomIndexQuote].author;
+    let newQuote = quotes[randomIndexQuote].text;
+    return [textQuote.text(newQuote), authorQuote.text(newAuthor)];
 };
 
 $(document).ready(() => {
+    showNewQuote();
     changeBackgroundColor();
     $("#new-quote").click(changeBackgroundColor);
+    $("#new-quote").click(showNewQuote);
 });
