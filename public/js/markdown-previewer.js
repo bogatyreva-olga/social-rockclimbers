19b26dd3afa390a7c1e1c3f32cb55e82ca8fe578
o.bogatyreva <bogatireva.o.v@gmail.com>
2023-10-14 23:52:32 +0300

    change event on input

--- public/js/markdown-previewer.js
+++ public/js/markdown-previewer.js
@@ -2,11 +2,12 @@ let md = window.markdownit();
 
 function showTextFromTextareaInBlockBelow() {
     let editor = $('#editor');
-
-    editor.on("change", () => {
+    let preview = $('#preview');
+    editor.on("input", () => {
         let editorValue = $('#editor').val();
         let result = md.render(editorValue);
-        $('#preview').append(result);
+        preview.empty();
+        preview.append(result);
     });
 }
 
