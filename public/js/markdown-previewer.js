07aa26a22dce5858182f210f413062542671402e
o.bogatyreva <bogatireva.o.v@gmail.com>
2023-10-14 22:53:19 +0300

    implemented render markdown

--- public/js/markdown-previewer.js
+++ public/js/markdown-previewer.js
@@ -1,12 +1,15 @@
-function showTextFromTextareaInBlockBelow() {
-let editor = $('#editor');
+let md = window.markdownit();
 
-editor.on("change", () => {
-    let editorValue = $('#editor').val();
-    $('#preview').append(editorValue);
-})
+function showTextFromTextareaInBlockBelow() {
+    let editor = $('#editor');
+
+    editor.on("change", () => {
+        let editorValue = $('#editor').val();
+        let result = md.render(editorValue);
+        $('#preview').append(result);
+    });
 }
 
 $(document).ready(() => {
-    showTextFromTextareaInBlockBelow()
-})
+    showTextFromTextareaInBlockBelow();
+});
